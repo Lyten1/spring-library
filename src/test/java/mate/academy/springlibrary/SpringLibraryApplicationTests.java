ac095c8e7c6a9ec58b5f3e0b1d87450b999e9d72
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-09-10 23:09:27 +0200

    initial commit for HW setUp

---
+++ src/test/java/mate/academy/springlibrary/SpringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
