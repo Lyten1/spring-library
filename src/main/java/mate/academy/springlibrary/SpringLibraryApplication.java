cc5ba2a89d16dbf60782574a74186b84d136951e
Vlad Makoda <71983986+Lyten1@users.noreply.github.com>
2025-09-15 11:29:26 +0200

    First steps, basic implementation of spring boot aplication (#1)
    * basic implementation of spring boot aplication
    
    * changes according to checklist
    
    * fixes after review
    
    * fixes styles
    
    * fixes after review v2
    
    * fixes after review v2

--- src/main/java/mate/academy/springlibrary/SpringLibraryApplication.java
+++ src/main/java/mate/academy/springlibrary/SpringLibraryApplication.java
@@ -1,13 +1,35 @@
 package mate.academy.springlibrary;
 
+import java.math.BigDecimal;
+import mate.academy.springlibrary.model.Book;
+import mate.academy.springlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringLibraryApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLibraryApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Red flag");
+            book.setAuthor("Jorge Orvel");
+            book.setIsbn("qwerty");
+            book.setPrice(new BigDecimal(12.99));
+
+            bookService.save(book);
+
+            bookService.findAll().forEach(System.out::println);
+        };
+    }
 }
