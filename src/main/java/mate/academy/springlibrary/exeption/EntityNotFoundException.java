b40804c61f75ad79e32f3011c40e9913e694e69d
Vlad Makoda <71983986+Lyten1@users.noreply.github.com>
2025-10-01 17:19:34 +0200

    first changes accoring to checklist (#3)
    * first changes accoring to checklist
    
    * fixes after review
    
    * fix styles

--- src/main/java/mate/academy/springlibrary/exeption/EntityNotFoundException.java
+++ src/main/java/mate/academy/springlibrary/exeption/EntityNotFoundException.java
@@ -1,6 +1,11 @@
 package mate.academy.springlibrary.exeption;
 
 public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
     public EntityNotFoundException(String message, Throwable e) {
         super(message, e);
     }
