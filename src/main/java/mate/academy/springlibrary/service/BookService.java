3173683182e5318b1855896897942d3af5c6af12
Vlad Makoda <71983986+Lyten1@users.noreply.github.com>
2025-10-03 11:37:32 +0200

    implemented pagable and swager (#6)
    * implemented pagable and swager
    
    * fixed merge

--- src/main/java/mate/academy/springlibrary/service/BookService.java
+++ src/main/java/mate/academy/springlibrary/service/BookService.java
@@ -4,12 +4,13 @@ import java.util.List;
 import mate.academy.springlibrary.dto.BookDto;
 import mate.academy.springlibrary.dto.BookSearchParametersDto;
 import mate.academy.springlibrary.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
 
     BookDto save(CreateBookRequestDto book);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
--- src/main/java/mate/academy/springlibrary/service/BookServiceImpl.java
+++ src/main/java/mate/academy/springlibrary/service/BookServiceImpl.java
@@ -10,6 +10,7 @@ import mate.academy.springlibrary.mapper.BookMapper;
 import mate.academy.springlibrary.model.Book;
 import mate.academy.springlibrary.repository.books.BookRepository;
 import mate.academy.springlibrary.repository.books.BookSpecificationBuilder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -27,8 +28,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
