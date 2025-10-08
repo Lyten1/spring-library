59cf82628d14959d0156ddd4fe3422dc260884b6
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-09 01:17:13 +0200

    fixes styles and liquidebase

--- src/main/java/mate/academy/springlibrary/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/springlibrary/security/CustomUserDetailsService.java
@@ -15,7 +15,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        return userRepository.findByEmail(email);
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Can't find user with email: " + email));
     }
 
 }
