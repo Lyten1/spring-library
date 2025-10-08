63e744ee107c5823798b46c623cac7cb0fa958e1
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-09 00:19:50 +0200

    fixes after review

--- src/main/java/mate/academy/springlibrary/repository/users/UserRepository.java
+++ src/main/java/mate/academy/springlibrary/repository/users/UserRepository.java
@@ -1,12 +1,10 @@
 package mate.academy.springlibrary.repository.users;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import mate.academy.springlibrary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(@NotBlank @Email String email);
+    boolean existsByEmail(String email);
 }
