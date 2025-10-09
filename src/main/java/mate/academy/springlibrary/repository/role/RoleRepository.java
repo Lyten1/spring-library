3e765d40930f019b5555c810fcf5578adafc4158
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-09 23:01:31 +0200

    fixes after review

--- src/main/java/mate/academy/springlibrary/repository/role/RoleRepository.java
+++ src/main/java/mate/academy/springlibrary/repository/role/RoleRepository.java
@@ -1,7 +1,10 @@
 package mate.academy.springlibrary.repository.role;
 
+import java.util.Optional;
 import mate.academy.springlibrary.model.Role;
+import mate.academy.springlibrary.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName name);
 }
