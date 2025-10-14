6c3d51d09c9037dd28c402fdbde9ba9e41302027
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-14 22:32:04 +0200

    add all modified files

--- src/main/java/mate/academy/springlibrary/repository/category/CategoryRepository.java
+++ src/main/java/mate/academy/springlibrary/repository/category/CategoryRepository.java
@@ -1,12 +1,9 @@
 package mate.academy.springlibrary.repository.category;
 
-import java.util.List;
-import mate.academy.springlibrary.model.Book;
 import mate.academy.springlibrary.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    List<Book> findAllByCategoryId(Long categoryId);
 }
