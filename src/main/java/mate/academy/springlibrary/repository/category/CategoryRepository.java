a1be64be9502be98bd3bde2327fbb8adc2204d00
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-21 01:04:53 +0200

    fix style

--- src/main/java/mate/academy/springlibrary/repository/category/CategoryRepository.java
+++ src/main/java/mate/academy/springlibrary/repository/category/CategoryRepository.java
@@ -1,14 +1,13 @@
 package mate.academy.springlibrary.repository.category;
 
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.Set;
 import mate.academy.springlibrary.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
