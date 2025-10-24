90387353993f67558e25e434ad40d6adfcf33a81
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-10-24 10:11:32 +0200

    delete redudant query

--- src/main/java/mate/academy/springlibrary/repository/cartitem/CartItemRepository.java
+++ src/main/java/mate/academy/springlibrary/repository/cartitem/CartItemRepository.java
@@ -2,7 +2,6 @@ package mate.academy.springlibrary.repository.cartitem;
 
 import mate.academy.springlibrary.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
